3e5f20837b4248461c70d0681f6b5b36106f3811
Adrian Alic <contact@alic.dev>
2023-05-19 00:00:00 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
 #![allow(unused)]
+#![feature(inline_const)]
+#![feature(const_trait_impl)]
 
 use std::arch::aarch64::*;
 
 #[rustfmt::skip]
 fn main() { unsafe { main_ (); } }
+pub trait ToArray<T: Default, const N: usize> {
+    fn to_array(self) -> [T; N];
+}
+impl<T: Default, const N: usize, I: Iterator<Item = T>> ToArray<T, N> for I {
+    fn to_array(self) -> [T; N] {
+        let mut x: [T; N] = std::array::from_fn(|_| Default::default());
+        for (idx, elem) in self.enumerate() {
+            x[idx] = elem;
+        }
+        x
+    }
+}
+/// Transposes an array into a table for vectorized lookups
+pub trait TransposeTable<T, const N: usize> {
+    fn transpose_table(&self, register_count: usize) -> Self;
+}
+impl<T: Clone, const N: usize> TransposeTable<T, N> for [T; N] {
+    fn transpose_table(&self, register_count: usize) -> Self {
+        std::array::from_fn(|i| {
+            let offset = i / register_count;
+            let idx = ((i % register_count) * 16 + offset);
+            self[idx].clone()
+        })
+    }
+}
 unsafe fn printx<T>(t: T) {
     let x: u128 = unsafe { *std::mem::transmute::<&T, &u128>(&t) };
-    println!("{:0>32x}", x);
+    let x: [u8; 16] = unsafe { *std::mem::transmute::<&T, &[u8; 16]>(&t) };
+    for i in x {
+        print!("{:0>2x} ", i);
+    }
+    println!("");
 }
 unsafe fn printb<T>(t: T) {
     let x: [u8; 16] = unsafe { *std::mem::transmute::<&T, &[u8; 16]>(&t) };
@@ -47,6 +78,47 @@ unsafe fn proc2() {
         panic!("nope");
     }
 }
+// table lookup
+unsafe fn proc3() {
+    let index_bytes: [u8; 16] = (0..16).map(|x| x / 4).to_array();
+    let dst_bytes: [u8; 16] = (0..16).rev().to_array();
+
+    let v1 = vld1q_u8(&index_bytes[0]);
+    let dst = vld1q_u8(&dst_bytes[0]);
+    let v2 = vqtbl1q_u8(dst, v1);
+    printx(v1);
+    printx(dst);
+    println!("indexing:");
+    printx(v2);
+}
+unsafe fn proc4() {
+    //assume ascii
+    let bytes = "ln foo(x: u64, h^: * -> *) { /*  def */ }".as_bytes();
+
+    // this is a lookup table for ASCII conversions (WIP)
+    // Note that the maximum 4 registers can hold is 64 bytes
+    let conv_table1: [u8; 64] = (0..64).to_array().transpose_table(4);
+    let conv_table2: [u8; 64] = (64..128).to_array().transpose_table(4);
+
+    let v192 = vld1q_dup_u8(&0xc0);
+    // read first 16 characters into register
+    let v1 = vld1q_u8(&bytes[0]);
+    // This shifts indices down by 64. This allows us to index exclusively
+    // the upper 64 ASCII characters.
+    let v1_upper = vaddq_u8(v1, v192); // v1[i] = v[i] - 64;
+
+    // map lower ascii characters (indices >=64 get ignored)
+    let conv1 = vld4q_u8(&conv_table1[0]);
+    // map upper ascii characters (indices < 64 get ignored)
+    let conv2 = vld4q_u8(&conv_table2[0]);
+
+    let index_1 = vqtbl4q_u8(conv1, v1);
+    let index_2 = vqtbl4q_u8(conv2, v1_upper);
+
+    printx(v1);
+    printx(index_1);
+    printx(index_2);
+}
 unsafe fn main_() {
-    proc2();
+    proc4();
 }
